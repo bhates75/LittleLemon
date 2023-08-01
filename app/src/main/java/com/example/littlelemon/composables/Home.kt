f86b8c3b043b74ce2e18f1cfb0d16fc968dc8024
Abid Hussain <abid.geek@gmail.com>
2023-07-31 21:03:29 -0400

    Implemented Hero Section, Filtering by Name, and Filtering by Category

--- app/src/main/java/com/example/littlelemon/composables/Home.kt
+++ app/src/main/java/com/example/littlelemon/composables/Home.kt
@@ -10,36 +10,67 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.littlelemon.LLDatabase
+import com.example.littlelemon.MenuItemRoom
 import com.example.littlelemon.Profile
 import com.example.littlelemon.R
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
-import com.example.littlelemon.MenuItemRoom
 
 @Composable
 fun Home(navController: NavHostController, database: LLDatabase){
-    val menuItems by database.menuItemDao().getAll().observeAsState(emptyList())
+    val databaseMenuItems by database.menuItemDao().getAll().observeAsState(emptyList())
+    var searchText by remember {
+        mutableStateOf("")
+    }
+    var currentCategory by remember {
+        mutableStateOf("")
+    }
+    var currentElem by remember {
+        mutableStateOf("")
+    }
+
+     var menuItems: List<MenuItemRoom> =
+             if (searchText.isNotEmpty()) {
+                 databaseMenuItems.filter { it.title.contains(searchText, ignoreCase = true) }
+             }
+             else if(currentCategory.isNotEmpty()){
+                 databaseMenuItems.filter { it.category.contains(currentCategory, ignoreCase = true) }
+             }
+             else{
+                 databaseMenuItems
+             }
+
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -69,7 +100,82 @@ fun Home(navController: NavHostController, database: LLDatabase){
                 )
             }
         }
+        HeroSection(searchText, currentCategory, currentElem, { typedText: String -> searchText = typedText }, {categoryName: String ->
+                currentCategory = categoryName
+        })
         MenuItemsDisplay(mItems = menuItems)
+    }
+}
+
+@Composable
+fun HeroSection(searchText: String, currCat: String, currElem: String, changeSearchText: (String) -> Unit, sortIntoCategory: (String) -> Unit){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(.45f)
+            .background(Color.DarkGray)
+    ){
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Text(
+                text = "Little Lemon",
+                fontSize = 44.sp,
+                color = Color.Yellow,
+                modifier = Modifier.offset(y = -10.dp)
+            )
+            Text(
+                text = "Chicago",
+                fontSize = 30.sp,
+                color = Color.LightGray,
+                modifier = Modifier.offset(y = -10.dp)
+            )
+            Text(
+                text = "We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist " + currCat + " " + currElem,
+                fontSize = 18.sp,
+                color = Color.White,
+                modifier = Modifier.padding(top = 10.dp, bottom = 30.dp)
+            )
+            OutlinedTextField(
+                value = searchText,
+                onValueChange = { changeSearchText(it) },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                placeholder = {Text(text = "Enter Search Query")}
+            )
+        }
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(.3f)
+            .background(Color.White)
+            .padding(start = 20.dp, end = 10.dp)
+    ) {
+        Column(){
+            Text(text = "Order for Delivery!")
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ){
+                items(
+                    items = listOf("Starters", "Mains", "Desserts", "Drinks"),
+                    itemContent = {category ->
+                        Button(
+                            onClick = {
+                                            sortIntoCategory(category)
+                                      },
+                            modifier = Modifier
+                                .fillMaxWidth(.2f)
+                                .padding(end = 10.dp),
+                            shape = RoundedCornerShape(40.dp),
+                        ) {
+                            Text(text = category)
+                        }
+                    }
+                )
+            }
+        }
     }
 }
 
@@ -78,7 +184,7 @@ fun MenuItemsDisplay(mItems: List<MenuItemRoom>){
     LazyColumn(
         modifier = Modifier
             .fillMaxHeight()
-            .padding(20.dp)
+            .padding(start = 20.dp, end = 20.dp)
     ){
         items(
             items = mItems,
@@ -101,20 +207,28 @@ fun MenuItemsDisplay(mItems: List<MenuItemRoom>){
                         fontWeight = FontWeight.SemiBold,
                         modifier = Modifier.padding(top = 5.dp,bottom = 5.dp)
                     )
-                    Text(
-                        text = menuItem.description,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Light,
+                    Row(
                         modifier = Modifier
-                            .fillMaxWidth(.7f)
-                            .padding(bottom = 5.dp)
-                    )
+                            .fillMaxWidth()
+                            .background(Color.Green)
+                    ) {
+                        Text(
+                            text = menuItem.description,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Light,
+                            modifier = Modifier
+                                .fillMaxWidth(.6f)
+                                .padding(bottom = 5.dp)
+                                .background(Color.Yellow)
+                        )
+                    }
                     Text(
                         text = "$" + menuItem.price + ".00",
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Medium,
                         modifier = Modifier.padding(bottom = 5.dp)
                     )
+
                 }
             }
         )
